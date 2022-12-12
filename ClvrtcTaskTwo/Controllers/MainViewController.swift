6c5e8803d4a29a8668f883d216f648f94836256c
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2022-12-12 21:25:08 +0300

    Initial project setup

--- ClvrtcTaskTwo/Controllers/MainViewController.swift
+++ ClvrtcTaskTwo/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ClvrtcTaskTwo
 //
 //  Created by Artyom Beldeiko on 12.12.22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    
+    
 }
-
