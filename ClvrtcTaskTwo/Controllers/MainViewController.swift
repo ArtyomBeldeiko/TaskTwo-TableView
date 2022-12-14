2700ea773b8343f32c2eb58d2439807b2121ef47
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2022-12-14 21:49:42 +0300

    ADDED UILongPressGestureRecognizer IMPLEMENTED possibility of cell deletion and movement

--- ClvrtcTaskTwo/Controllers/MainViewController.swift
+++ ClvrtcTaskTwo/Controllers/MainViewController.swift
@@ -33,12 +33,14 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture))
+        
         addSubviews()
         setConstraints()
         
         mainTableView.dataSource = self
         mainTableView.delegate = self
-        
+        mainTableView.addGestureRecognizer(longPressGesture)
     }
     
     private func addSubviews() {
@@ -64,6 +66,12 @@ class MainViewController: UIViewController {
         NSLayoutConstraint.activate(backgroundViewContraints)
         NSLayoutConstraint.activate(mainTableViewConstraints)
     }
+    
+//    MARK: - Actions
+    
+    @objc private func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
+        mainTableView.isEditing = true
+    }
 }
 
 // MARK: - UITableViewDataSource, UITableViewDelegate
@@ -75,13 +83,33 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MainTableViewCell.identifier, for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
-        
         cell.titleLable.text = "Title \(data[indexPath.row].title)"
         cell.descriptionLable.text = "Description \(data[indexPath.row].description)"
         cell.leadingImageView.image = data[indexPath.row].image
         return cell
     }
     
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            data.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        data.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        tableView.isEditing = false
+    }
 }
 
 
